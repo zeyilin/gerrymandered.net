2e6c69d5db79f69e0decb123e8d63524053ac368
Sami Glasco <samiglasco@gmail.com>
2016-01-16 16:56:05 -0600

    oops

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(submitLocation);
     } else {
         document.getElementById("loc").innerHTML = "Geolocation is not supported by this browser.";
     }
@@ -10,48 +10,19 @@ function showPosition(position) {
     document.getElementById("loc").innerHTML = "Latitude: " + position.coords.latitude +
         "<br>Longitude: " + position.coords.longitude;
 
-    var params = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude,
-        username: 'gangoffour'
-    };
-
-    $.get('http://api.geonames.org/findNearbyPostalCodesJSON', params, function (result) {
-        $('#zip').html(result['postalCodes'][0]['postalCode']);
-    });
+    //var params = {
+    //    lat: position.coords.latitude,
+    //    lng: position.coords.longitude,
+    //    username: 'gangoffour'
+    //};
+    //
+    //$.get('http://api.geonames.org/findNearbyPostalCodesJSON', params, function (result) {
+    //    $('#zip').html(result['postalCodes'][0]['postalCode']);
+    //});
 }
 
 function submitLocation(position) {
-    var params = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude,
-        username: 'gangoffour'
-    };
 
-    $.ajax({
-        async: false,
-        data: params,
-        type: 'GET',
-        url: 'http://api.geonames.org/findNearbyPostalCodesJSON',
-        success: function (data) {
-            $('#address-input').val(data['postalCodes'][0]['postalCode']);
-            $('#address-form').submit();
-        }
-    });
-
-
-    //document.forms["address-form"].submit();
-}
-
-function loadAddress() {
-    var address = getUrlVars()["address-input"].replace(/\+/g, " ");
-    document.getElementById('address').innerHTML = address;
-}
-
-function getUrlVars() {
-    var vars = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
-        vars[key] = value;
-    });
-    return vars;
+    $('#address-input').val(position.coords.latitude +"____" + position.coords.longitude);
+    $('#address-form').submit();
 }
